f1f8678ed06bc1399ef853aa73feab09a7630f02
yonlin zhu <zylin0721@foxmail.com>
2021-01-20 18:18:57 +0800

    .

---
+++ MarketOnline.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOnline.Core
+{
+    public class Class1
+    {
+    }
+}
