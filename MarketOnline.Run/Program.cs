c5f174f810e2435abfe48ac6d649f60a420381b4
yonlin <zylin0721@foxmail.com>
2021-02-25 23:11:57 +0800

    .

---
+++ MarketOnline.Run/Program.cs
@@ -0,0 +1,22 @@
+﻿using MarketOnline.Core.Infrastructure;
+using MarketOnline.Core.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarketOnline.Run
+{
+    class Program
+    {
+        public static object Theading { get; private set; }
+
+        static void Main(string[] args)
+        {
+            InitialEngine.Start();
+            Console.ReadLine();
+        }
+    }
+}
--- MarketOnline.Test/Class1.cs
+++ MarketOnline.Test/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using WebSocketSharp;
 
 namespace Example
@@ -11,12 +12,21 @@ namespace Example
         {
             using (var ws = new WebSocket("wss://stream.binance.com:9443/ws"))
             {
+                var count = 0;
                 ws.OnMessage += (sender, e) =>
-                    Console.WriteLine("Laputa says: " + e.Data);
+                {
+                    Console.WriteLine(e.Data);
+                    count++;
+                    if (count == 2) ws.Close();
+
+                };
                 ws.OnOpen += (sender, e) => Console.WriteLine("连接成功！");
                 ws.SetProxy("http://127.0.0.1:10809", "", "");
                 ws.Connect();
-                ws.Send(@"{ ""method"": ""SUBSCRIBE"", ""params"": [ ""btcusdt@bookTicker""], ""id"": 1 }");
+                //ws.Send(@"{ ""method"": ""SUBSCRIBE"", ""params"": [ ""btcusdt@bookTicker""], ""id"": 1 }");
+                ws.Send(@"{ ""method"": ""SUBSCRIBE"", ""params"": [ ""!miniTicker@arr""], ""id"": 1 }");
+                //ws.Send(@"{ ""method"": ""SUBSCRIBE"", ""params"": [ ""btcusdt@kline_4h""], ""id"": 1 }");
+                //ws.Close();
                 Console.ReadKey(true);
             }
         }
