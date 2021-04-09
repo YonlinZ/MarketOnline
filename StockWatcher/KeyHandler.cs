476bc40c16bd3231b6895b13966a818689486a2e
yonlin zhu <zylin0721@foxmail.com>
2021-04-09 15:41:56 +0800

    .

--- StockWatcher/KeyHandler.cs
+++ StockWatcher/KeyHandler.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StockWatcher
@@ -13,15 +9,31 @@ namespace StockWatcher
         private static DateTime _dt = DateTime.MinValue;
         public static void Start(Control control)
         {
-            var hook = new KeyboardHook();
-            hook.KeyDownEvent += (sender, e) =>
+            try
             {
-                if (e.KeyValue == (int)Keys.A && (int)Control.ModifierKeys == (int)Keys.Control)
+                var hook = new KeyboardHook();
+                hook.KeyDownEvent += (sender, e) =>
                 {
-                    control.Invoke((Action)(() => control.Visible = !control.Visible));
-                }
-            };
-            hook.Start();
+                    try
+                    {
+                        if (e.KeyValue == (int)Keys.A && (int)Control.ModifierKeys == (int)Keys.Control)
+                        {
+                            IntPtr i = control.Handle;
+                            control.Invoke((Action)(() => control.Visible = !control.Visible));
+                            Util.Log("按下老板键 Ctrl + A");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Util.Log(ex.Message);
+                    }
+                };
+                hook.Start();
+            }
+            catch (Exception ex)
+            {
+                Util.Log(ex.Message);
+            }
         }
     }
 }
